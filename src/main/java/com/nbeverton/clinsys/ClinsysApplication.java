74fce1a0e07732d48d97f4d5def7335aec287435
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-06-27 21:09:57 -0300

    first commit

---
+++ src/main/java/com/nbeverton/clinsys/ClinsysApplication.java
@@ -0,0 +1,13 @@
+package com.nbeverton.clinsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbeverton/clinsys/ClinsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbeverton.clinsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
