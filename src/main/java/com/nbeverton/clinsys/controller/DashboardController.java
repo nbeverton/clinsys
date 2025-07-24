21b92a33dff87c05254aa9430f59b71395d97e64
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-07-24 18:52:31 -0300

    Create DashboardController.java
    Criado o DashboardController
    
    - Testado. Funcionando corretamente.

---
+++ src/main/java/com/nbeverton/clinsys/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package com.nbeverton.clinsys.controller;
+
+import com.nbeverton.clinsys.dto.DashboardDTO;
+import com.nbeverton.clinsys.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    public ResponseEntity<DashboardDTO> getDashboardData(){
+        DashboardDTO dto = dashboardService.getDashboardData();
+        return ResponseEntity.ok(dto);
+    }
+
+}
