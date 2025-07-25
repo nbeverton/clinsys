186977eb90e5b90eb8f5aecfe9fddb2ee7ed0a44
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-07-25 17:06:27 -0300

    Update AppointmentDTO.java
    Pequena correção de rota.
    
    - Testado - Todas as rotas e requisições funcionando perfeitamente.

--- src/main/java/com/nbeverton/clinsys/dto/AppointmentDTO.java
+++ src/main/java/com/nbeverton/clinsys/dto/AppointmentDTO.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Data
 public class AppointmentDTO {
 
-    @NotBlank(message = "O campo data é obrigatório")
+    @NotNull(message = "O campo data é obrigatório")
     private LocalDate date;
 
     @NotBlank(message = "O campo hora é obrigatório")
